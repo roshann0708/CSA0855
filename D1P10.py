37b1ac77c9f2664a697c3c27601352223e854083
roshann0708 <125014249+roshann0708@users.noreply.github.com>
2023-02-13 09:48:15 +0530

    Update and rename Day 1 p10.py to D1P10.py

---
+++ D1P10.py
@@ -0,0 +1,19 @@
+def ad(s):
+    f=[0]*26
+    n=len(s)
+    for i in range(n):
+        f[ord(s[i])-ord('a')]=1
+                               
+    for i in range(n):
+        add=f[ord(s[i])-ord('a')]%26
+        if(ord(s[i])+add<=ord('z')):
+            s[i]=chr(ord(s[i])+add)
+        else:
+            add=(ord(s[i])+add)-(ord('z'))
+            s[i]=chr(ord('a')+add-1)
+    print(" ".join(s))
+
+x=input("Enter a string-")
+ad([i for i in x])
+                               
+                               
