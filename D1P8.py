6e04380bc3c986aac6b4c9f0293bef87683ff766
roshann0708 <125014249+roshann0708@users.noreply.github.com>
2023-02-13 09:35:00 +0530

    Create D1P8.py

---
+++ D1P8.py
@@ -0,0 +1,31 @@
+def isNumber(s):
+    s = s.strip()
+    if not s:
+      return False
+
+    foundNum = False
+    foundDot = False
+    foundE = False
+
+    for i, c in enumerate(s):
+      if c == '.':
+        if foundDot or foundE:
+          return False
+        foundDot = True
+      elif c.lower() == 'e':
+        if foundE or not foundNum:
+          return False
+        foundE = True
+        foundNum = False
+      elif c in '+-':
+        if i > 0 and s[i - 1] != 'e':
+          return False
+        foundNum = False
+      else:
+        if not c.isdigit():
+          return False
+        foundNum = True
+
+    return foundNum
+x=input("E=")
+print(isNumber(x))
