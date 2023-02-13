20977c740224d3557d1034f4fcfb0b31991e66ad
roshann0708 <125014249+roshann0708@users.noreply.github.com>
2023-02-13 08:26:14 +0530

    Update Day 1 p1.py

--- Day 1 p1.py
+++ Day 1 p1.py
@@ -1,17 +1,24 @@
-s=input("s= ")
-t=input("t=")
-k=ord(s[0])-ord(t[0])
-print(k)
-l=0
-if len(s)==len(t):
-    for i in range(1,len(s)):
-        if((ord(s[i])-ord(t[i]))==k):
-            l=l+1
-    if(l==(len(s)-1)):
-       print("TRUE")
-    else:
-        print("FALSE")
-else:
-    print("FALSE")
+MAX_CHARS = 256
+def areiso(s1, s2):
+    m=len(s1)
+    n=len(s2)
+    if m!= n:
+        return False
+    marked =[False]
+    marked=[False]*MAX_CHARS
+    map=[-1]*MAX_CHARS
+    for i in range(n):
+        if map[ord(s1[i])]==-1:
+            if marked[ord(s2[i])]==True:
+                return False
+            marked[ord(s2[i])]=True
+            map[ord(s1[i])]=s2[i]
+        elif map[ord(s1[i])]!=s2[i]:
+            return False
+    return True
 
-            
+x=input("X=")
+y=input("Y=")
+z=areiso(x,y)
+
+print(z)
